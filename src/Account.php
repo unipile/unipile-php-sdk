4f5506bb70f17b06781e7329a8f66d136754c1cf
Arnaud Hartmann <arnaud.hartmann@gmail.com>
2023-10-13 16:15:00 +0200

    add api_url parameter

--- src/Account.php
+++ src/Account.php
@@ -169,6 +169,7 @@ class Account extends UnipileSDK
         try {
             $response = $this->httpClient->post('/api/v1/hosted/accounts/link', [
                 'json' => [
+                    'api_url' => $this->baseUri,
                     'type' => $type,
                     'name' => $name,
                     'providers' => $providers,
